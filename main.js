7e67a3475fb1622e5d59c7f1dd849463f11adca2
Anil Sapkota <muzicmaniac2015@gmail.com>
2022-02-21 16:35:20 -0600

    kelvinproject

---
+++ main.js
@@ -0,0 +1,11 @@
+//let us assign a variable kelvin to be of value 293
+let kelvin =0;
+
+//convert kelvin to celcius and storing it in a variable
+let celcius = kelvin -273; 
+
+//calculate the fahrenheit from the celcius and storing it in a variable and flooring it. 
+let fahrenheit = Math.floor(celcius *(9/5) +32);
+
+//loging out the value in the console
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
